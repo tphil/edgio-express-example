9cc149d8d9b5733581a420d2dcdc0dd6a626cd62
Tristan Lee <tristan.lee@moovweb.com>
2022-11-28 10:04:08 -0500

    Upgrade to Edgio v5

--- edgio.config.js
+++ edgio.config.js
@@ -1,12 +1,12 @@
-// This file was automatically added by layer0 deploy.
+// This file was automatically added by edgio deploy.
 // You should commit this file to source control.
 module.exports = {
-  connector: '@layer0/express',
+  connector: '@edgio/express',
   express: {
     // The main entry point(s) for your app, which exports an instance of express app.
     // This file and its dependencies will be bundled into a single file for serverless deployment.
     //
-    // If omitted, Layer0 will try to find an entrypoint in one of the following files:
+    // If omitted, Edgio will try to find an entrypoint in one of the following files:
     // - ./src/server.ts
     // - ./src/server.js
     // - ./src/app.ts
--- routes.js
+++ routes.js
@@ -1,5 +1,5 @@
-// This file was automatically added by layer0 deploy.
+// This file was automatically added by edgio deploy.
 // You should commit this file to source control.
-import { Router } from '@layer0/core'
+import { Router } from '@edgio/core'
 
 export default new Router().fallback(({ renderWithApp }) => renderWithApp())
